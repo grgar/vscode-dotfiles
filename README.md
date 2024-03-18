dbf2bca8826574a1c5c76c62e733dbcde85c1efa
George Garside <apps@georgegarside.com>
2024-03-18 21:49:43 +0000

    Add support for multiple files
    Use an object as config where each key-value is a file to be written.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-
 # dotfiles
 
 Configure dotfiles from your VS Code settings so they're always available.
+
+## Extension Settings
+
+* `dotfiles.directory`: Base directory path for all config files to be written.
+* `dotfiles.files`: Files to be written to the configured directory, where key is the relative path to the file and value is the content of the file.
--- src/extension.ts
+++ src/extension.ts
@@ -6,20 +6,21 @@ const configNamespace = "dotfiles";
 
 async function apply() {
 	const config = vscode.workspace.getConfiguration(configNamespace);
-	const directory = config.get<string>("directory") || process.env["XDG_CONFIG_DIR"] || path.join(process.env["HOME"]!, ".config");
-	const filePath = path.join(directory, config.get<string>("file") || "fish/conf.d/vscode.fish");
-	fs.stat(filePath, (err, _) => {
-		if (err) {
-			vscode.window.showWarningMessage(`${filePath} does not exist`);
-			return;
-		}
-		const data = config.get<string>("content");
-		if (!data) {
-			vscode.window.showInformationMessage("No content to write");
-			return;
-		}
-		fs.writeFile(filePath, data, { encoding: "utf-8", mode: "w" }, () => { });
-	});
+	const directory = config.get<string>("directory", process.env["XDG_CONFIG_DIR"] ?? path.join(process.env["HOME"]!, ".config"));
+	const files = Object.entries(config.get<object>("files", {}));
+	for (const [file, content] of files) {
+		const filePath = path.join(directory, file);
+		fs.stat(filePath, (err, _) => {
+			if (err) {
+				vscode.window.showWarningMessage(`${filePath} does not exist`);
+				return;
+			}
+			if (!content) {
+				return;
+			}
+			fs.writeFile(filePath, content, { encoding: "utf-8", mode: "w" }, () => { });
+		});
+	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
