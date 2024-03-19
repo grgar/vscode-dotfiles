0c0156c8a91638e913d99fa224d99beae4a5fdc2
George Garside <apps@georgegarside.com>
2024-03-19 20:52:20 +0000

    Fix command identifier
    Use ID of extension as prefix in command ID and title.

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog
+### v0.1.1
+
+- Fix command identifier
+  
+  Use ID of extension as prefix in command ID and title.
+
+### v0.1.0
+
+- Create LICENSE
+
+
+- Add support for multiple files
+  
+  Use an object as config where each key-value is a file to be written.
+
+
+- Scaffold to apply one file from settings
+  
+  Adds some configuration options to write one file from settings to the configured (or defaulted) dotfiles folder.
+
+
+- Initial commit
+
--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ const configNamespace = "dotfiles";
 
 async function apply() {
 	const config = vscode.workspace.getConfiguration(configNamespace);
-	const directory = config.get<string>("directory", process.env["XDG_CONFIG_DIR"] ?? path.join(process.env["HOME"]!, ".config"));
+	const directory = config.get<string>("directory") || process.env["XDG_CONFIG_DIR"] || path.join(process.env["HOME"]!, ".config");
 	const files = Object.entries(config.get<object>("files", {}));
 	for (const [file, content] of files) {
 		const filePath = path.join(directory, file);
@@ -24,7 +24,7 @@ async function apply() {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('config-setup.apply', apply));
+	context.subscriptions.push(vscode.commands.registerCommand("dotfiles.apply", apply));
 }
 
 export function deactivate() { }
