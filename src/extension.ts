a96825235bf318abb4c5a7a9afc9d1bcc928bb02
George Garside <apps@georgegarside.com>
2024-03-21 15:17:59 +0000

    Optionally automatically update dotfiles to and from settings
    Add a new config option to automatically apply files from settings, and to apply changes to the files back to settings.

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,34 @@ import * as vscode from 'vscode';
 const configNamespace = "dotfiles";
 const outputChannel = vscode.window.createOutputChannel(configNamespace);
 
+function getDirectoryPath() {
+	let directory = vscode.workspace.getConfiguration(configNamespace).get<string>("directory") || process.env["XDG_CONFIG_DIR"] || path.join(process.env["HOME"]!, ".config");
+	if (directory.endsWith("/")) {
+		return directory.slice(directory.length - 1);
+	}
+	return directory;
+}
+
+function getFiles() {
+	return vscode.workspace.getConfiguration(configNamespace).get<{ [key: string]: string; }>("files", {});
+}
+
+async function setFiles(files: { [key: string]: string; }) {
+	try {
+		await vscode.workspace.getConfiguration(configNamespace).update("files", files, vscode.ConfigurationTarget.Global);
+	} catch (err) {
+		outputChannel.appendLine(`${new Date().toLocaleString()}: failed to update files: ${err}`);
+		vscode.window.showErrorMessage("Unable to update dotfiles.files: " + err);
+	}
+}
+
+function shouldAutoUpdate() {
+	return vscode.workspace.getConfiguration(configNamespace).get<boolean>("autoUpdate", false);
+}
+
 async function apply() {
-	const config = vscode.workspace.getConfiguration(configNamespace);
-	const directory = config.get<string>("directory") || process.env["XDG_CONFIG_DIR"] || path.join(process.env["HOME"]!, ".config");
-	const files = Object.entries(config.get<object>("files", {}));
-	for (const [file, content] of files) {
+	const directory = getDirectoryPath();
+	for (const [file, content] of Object.entries(getFiles())) {
 		const filePath = path.join(directory, file);
 		try {
 			await fs.stat(filePath);
@@ -21,8 +44,36 @@ async function apply() {
 	}
 }
 
+function didSave(doc: vscode.TextDocument) {
+	if (!shouldAutoUpdate()) {
+		return;
+	}
+	if (doc.uri.scheme !== "file") {
+		return;
+	}
+	const directory = getDirectoryPath();
+	if (!doc.uri.path.startsWith(directory + "/")) {
+		return;
+	}
+	const files = getFiles();
+	const relativePath = doc.uri.path.slice(directory.length + 1);
+	if (!files[relativePath]) {
+		outputChannel.appendLine(`${new Date().toLocaleString()}: file ${relativePath} in ${directory} not found in settings`);
+		return;
+	}
+	files[relativePath] = doc.getText();
+	setFiles(files);
+	outputChannel.appendLine(`${new Date().toLocaleString()}: updated ${relativePath}`);
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand("dotfiles.apply", apply));
+	context.subscriptions.push(
+		vscode.commands.registerCommand("dotfiles.apply", apply),
+		vscode.workspace.onDidSaveTextDocument(didSave)
+	);
+	if (shouldAutoUpdate()) {
+		apply();
+	}
 }
 
 export function deactivate() { }
